f9a43864f58ebeeb32315f34ef58526752e9c050
haxuanthuyen <haxuanthuyen@gmail.com>
2021-05-08 23:48:55 +0700

    Create dbt-demo-dag.py

---
+++ dbt-demo-dag.py
@@ -0,0 +1,37 @@
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import datetime
+from airflow.utils.dates import timedelta
+
+default_args = {
+    'owner': 'thuyen.ha',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 5, 5),
+    'email': ['thuyen.ha@onemount.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+dag = DAG(
+    'dbt_dag_test_2',
+    default_args=default_args,
+    description='An Airflow DAG to invoke simple dbt commands',
+    schedule_interval=timedelta(days=1),
+)
+
+dbt_run = BashOperator(
+    task_id='dbt_run',
+    bash_command='dbt --version',
+    dag=dag
+)
+
+dbt_test = BashOperator(
+    task_id='dbt_test',
+    bash_command='dbt --version',
+    dag=dag
+)
+
+dbt_run >> dbt_test
